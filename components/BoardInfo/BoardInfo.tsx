f4336b545cb0ffc9f04a41e577ab676f8415aeb2
Dmitri Calmîș <calmis87@gmail.com>
2022-11-09 11:03:39 +0200

    add svgs

---
+++ components/BoardInfo/BoardInfo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Board, Clock } from "../../svg";
+
+export const BoardInfo = () => {
+  return (
+    <div className="board-info">
+      <Board />
+      BoardInfo
+      <Clock />
+    </div>
+  );
+};
---
+++ svg/Search.tsx
@@ -0,0 +1,19 @@
+export const Search = () => {
+  return (
+    <svg
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
